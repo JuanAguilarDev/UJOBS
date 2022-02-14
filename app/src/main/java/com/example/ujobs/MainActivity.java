4c37c710e06954aa873dcb9fe03b86dd345da84a
JuanAguilarDev <juan_aguilar1290@outlook.es>
2022-02-13 18:48:35 -0600

    "Second commit"

--- app/src/main/java/com/example/ujobs/MainActivity.java
+++ app/src/main/java/com/example/ujobs/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.ujobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button register;
+    Button auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.btnRegister);
+        auth = findViewById(R.id.btnAuth);
+    }
+
+    // Options Method
+
+    // Register
+    public void register(View view){
+        Intent register = new Intent(this, RegisterActivity.class);
+        startActivity(register);
+    }
+
+    // Login
+    public void auth(View view){
+        Intent auth = new Intent(this, AuthActivity.class);
+        startActivity(auth);
     }
 }
--- app/src/main/java/com/example/ujobs/RegisterActivity.java
+++ app/src/main/java/com/example/ujobs/RegisterActivity.java
@@ -3,12 +3,32 @@ package com.example.ujobs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnCreate;
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        sp = findViewById(R.id.sp);
+        btnCreate = findViewById(R.id.btnCrear);
+
+        String [] options = {"Guanajuato", "Michoacan"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
     }
+
+    
+
 }
