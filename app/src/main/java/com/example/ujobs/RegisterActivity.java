5a0c4ab5fad5ce538680d3f639ec31804c605c71
JuanAguilarDev <juan_aguilar1290@outlook.es>
2022-03-21 11:24:14 -0600

    "Update"

--- app/src/main/java/com/example/ujobs/RegisterActivity.java
+++ app/src/main/java/com/example/ujobs/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.ujobs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.PatternsCompat;
 
@@ -13,8 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ujobs.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -32,11 +38,15 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputLayout password;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         // Variables association
         sp = findViewById(R.id.sp);
@@ -71,17 +81,29 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(this, "Los datos no se han especificado de una correcta manera.", Toast.LENGTH_SHORT).show();
             return;
         }else{
-            User user = new User();
-            user.setUid(UUID.randomUUID().toString());
-            user.setName(name.getEditText().getText().toString().trim());
-            user.setEmail(email.getEditText().getText().toString().trim());
-            user.setState(sp.getSelectedItem().toString().trim());
-            user.setPassword(password.getEditText().getText().toString().trim());
-            databaseReference.child("User").child(user.getUid()).setValue(user);
-            Toast.makeText(this, "Usuario creado", Toast.LENGTH_SHORT).show();
-            Intent main = new Intent(this, MainActivity.class);
-            startActivity(main);
-            finish();
+            firebaseAuth.createUserWithEmailAndPassword(email.getEditText().getText().toString().trim(), password.getEditText().getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        User user = new User();
+                        user.setUid(UUID.randomUUID().toString());
+                        user.setName(name.getEditText().getText().toString().trim());
+                        user.setEmail(email.getEditText().getText().toString().trim());
+                        user.setState(sp.getSelectedItem().toString().trim());
+                        user.setPassword(password.getEditText().getText().toString().trim());
+                        databaseReference.child("User").child(user.getUid()).setValue(user);
+                        Toast.makeText(RegisterActivity.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+                        Intent main = new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(main);
+                        finish();
+                    }else{
+                        String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                        toastError(errorCode) ;
+                    }
+                }
+            });
+
+
         }
     }
 
@@ -140,8 +162,82 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
+    private void toastError(String error) {
 
+        switch (error) {
 
+            case "ERROR_INVALID_CUSTOM_TOKEN":
+                Toast.makeText(RegisterActivity.this, "El formato del token personalizado es incorrecto. Por favor revise la documentación", Toast.LENGTH_LONG).show();
+                break;
 
+            case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                Toast.makeText(RegisterActivity.this, "El token personalizado corresponde a una audiencia diferente.", Toast.LENGTH_LONG).show();
+                break;
 
+            case "ERROR_INVALID_CREDENTIAL":
+                Toast.makeText(RegisterActivity.this, "La credencial de autenticación proporcionada tiene un formato incorrecto o ha caducado.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_EMAIL":
+                Toast.makeText(RegisterActivity.this, "La dirección de correo electrónico está mal formateada.", Toast.LENGTH_LONG).show();
+                email.setError("La dirección de correo electrónico está mal formateada.");
+                email.requestFocus();
+                break;
+
+            case "ERROR_WRONG_PASSWORD":
+                Toast.makeText(RegisterActivity.this, "La contraseña no es válida o el usuario no tiene contraseña.", Toast.LENGTH_LONG).show();
+                password.setError("la contraseña es incorrecta ");
+                password.requestFocus();
+                break;
+
+            case "ERROR_USER_MISMATCH":
+                Toast.makeText(RegisterActivity.this, "Las credenciales proporcionadas no corresponden al usuario que inició sesión anteriormente..", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_REQUIRES_RECENT_LOGIN":
+                Toast.makeText(RegisterActivity.this, "Esta operación es sensible y requiere autenticación reciente. Inicie sesión nuevamente antes de volver a intentar esta solicitud.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                Toast.makeText(RegisterActivity.this, "Ya existe una cuenta con la misma dirección de correo electrónico pero diferentes credenciales de inicio de sesión. Inicie sesión con un proveedor asociado a esta dirección de correo electrónico.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_EMAIL_ALREADY_IN_USE":
+                Toast.makeText(RegisterActivity.this, "La dirección de correo electrónico ya está siendo utilizada por otra cuenta..   ", Toast.LENGTH_LONG).show();
+                email.setError("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
+                email.requestFocus();
+                break;
+
+            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                Toast.makeText(RegisterActivity.this, "Esta credencial ya está asociada con una cuenta de usuario diferente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_DISABLED":
+                Toast.makeText(RegisterActivity.this, "La cuenta de usuario ha sido inhabilitada por un administrador..", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_TOKEN_EXPIRED":
+                Toast.makeText(RegisterActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_NOT_FOUND":
+                Toast.makeText(RegisterActivity.this, "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_USER_TOKEN":
+                Toast.makeText(RegisterActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_OPERATION_NOT_ALLOWED":
+                Toast.makeText(RegisterActivity.this, "Esta operación no está permitida. Debes habilitar este servicio en la consola.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_WEAK_PASSWORD":
+                Toast.makeText(RegisterActivity.this, "La contraseña proporcionada no es válida..", Toast.LENGTH_LONG).show();
+                password.setError("La contraseña no es válida, debe tener al menos 6 caracteres");
+                password.requestFocus();
+                break;
+
+        }
+    }
 }
